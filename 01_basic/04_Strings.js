16be15ac1059e3689b27c59c6395361ebc59871b
SahilPatel010 <sahil9223538388@gmail.com>
2024-07-22 13:48:46 +0000

    CompletedStringProto

---
+++ 01_basic/04_Strings.js
@@ -0,0 +1,22 @@
+    // const name="Sahil Patel";
+    // const Subject="Python";
+    // const version=3;
+
+    // console.log(`I am ${name} and iam using ${Subject} ${version}`);
+
+    const gameName=new String("Rockstar-gta-10");
+
+    // console.log(gameName.toUpperCase());
+    // console.log(gameName.indexOf('g'));
+
+    const domain=gameName.substring(0,3)
+    // console.log(domain);
+
+    const domainNo=gameName.slice(-5)
+    // console.log(gameName);
+
+    // console.log(gameName.trim());
+
+    // console.log(gameName.includes('tata'));
+
+    console.log(gameName.split('-'));
