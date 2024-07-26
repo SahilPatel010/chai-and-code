5639ceab525d7fb3989fb5f2a541278073ebd1bb
SahilPatel010 <sahil9223538388@gmail.com>
2024-07-26 06:09:12 +0000

    mathsAndNumber  completed

---
+++ 01_basic/05_numAndMaths.js
@@ -0,0 +1,17 @@
+// const balance=new Number(100.44);
+// const newbalance=balance;
+// console.log(newbalance.toPrecision(3));
+
+
+// const hundreds=10000000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-34));
+// console.log(Math.round(4.55));
+// console.log(Math.ceil(34.5));
+// console.log(Math.floor(34.5));
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6));
+
+console.log(Math.random()*10 +1);
