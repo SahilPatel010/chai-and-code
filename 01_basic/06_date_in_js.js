20a3693d2652159da0fdfd8d48de09bc316c15ca
SahilPatel010 <sahil9223538388@gmail.com>
2024-07-26 12:18:30 +0000

    time and added

---
+++ 01_basic/06_date_in_js.js
@@ -0,0 +1,23 @@
+// let mydate=new Date();
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleTimeString('en-IN'));
+// console.log(mydate.toTimeString('en-IN'));
+
+// let mytimestamp=Date.now();
+
+// console.log(mytimestamp);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate=new Date();
+// console.log(newDate.getDay()+1);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getTime());
+// console.log(newDate.getMinutes());
+
+newDate.toLocaleString('default',{
+    weekday:"newDate"
+})
