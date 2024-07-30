c2cfbbea36295ce6a096b37b49e9baf7427162fc
SahilPatel010 <sahil9223538388@gmail.com>
2024-07-30 11:11:21 +0000

    array updated

---
+++ 01_basic/07_arrays.js
@@ -0,0 +1,12 @@
+// const jumble=[1,2,3,[4,5,6],[7,8,9]];
+
+// console.log(jumble.flat(2));
+
+// console.log(Array.isArray("Sahil"));
+// console.log(Array.from("Sahil"));
+// console.log(Array.from({name:"Sahil"}));
+
+let score1=1;
+let score2=2;
+let score3=3;
+console.log(Array.of(score1,score3));
