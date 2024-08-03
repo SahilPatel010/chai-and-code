24891d61a08f47f0ef2b704a92f1ac45f2329521
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-03 06:35:05 +0000

    object updated

---
+++ 02_basics/02_object.js
@@ -0,0 +1,30 @@
+const instaUser={
+    
+       
+            firstName:"Sahil",
+            LastName:"Patel",
+            age:20,
+        
+    
+};
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+console.log(instaUser.hasOwnProperty('LastName'));
+
+
+
+// instaUser.name="Sahil";
+// instaUser.age=20;
+// instaUser.isloggedin=true;
+
+// console.log(instaUser);
+
+// const target={1:"a",2:"b"}
+// const source={3:"c",4:"d"}
+// const source2={5:"c",6:"d"}
+// const source3={7:"c",8:"d"}
+// // const project=Object.assign({}, target,source,source2,source3)
+// const project={...target,...source,...source2,...source3}
+
+// console.log(project);
