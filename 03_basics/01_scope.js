cdc4790620bab883013300a9f49e5b1facbc81b0
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-09 13:31:27 +0000

    scope updated

--- 03_basics/01_scope.js
+++ 03_basics/01_scope.js
@@ -1,13 +1,50 @@
-let a=10;
-const b=20;
-var c=30;
+// let a=10;
+// const b=20;
+// var c=30;
 
-if (true){
-    let a=100;
-    const b=200;
-    var c=300;
-    console.log(c);
+// if (true){
+//     let a=100;
+//     const b=200;
+//     var c=300;
+//     console.log(c);
     
+// }
+// var c=400;
+// console.log(c);
+
+// function one() {
+//     const username="Sahil";
+//     const surname="Patel";
+//     console.log(surname);
+    
+// function two() {
+//     const website="youtube";
+//     console.log(username);    
+// } 
+// //console.log(website);
+// //two()
+// }
+// one()
+
+// if (true){
+//     const username="Ayan";
+//     if(username==="Ayan"){
+//         const surname="Shaikh"
+//         console.log(username + " " + surname); 
+//     }
+//   //  console.log(surname);
+    
+// }
+// //console.log(username);
+console.log(addOne(5));
+
+function addOne(num) {
+    return num+1
 }
-var c=400;
-console.log(c);
+
+
+
+const addTwo=function (num) {
+    return num+2;
+}
+console.log(addTwo(4));
