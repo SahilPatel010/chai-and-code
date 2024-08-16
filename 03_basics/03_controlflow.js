1a9e65b97b56a42fb1f27126acd7c86c08f2fbcb
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-16 12:12:49 +0000

    conditions agreed

---
+++ 03_basics/03_controlflow.js
@@ -0,0 +1,45 @@
+// if (2=='2'){
+//     console.log("executed");
+    
+// }
+
+// const temperatue=45;
+
+// if (temperatue>=50) {
+//     console.log("temp is very high");
+// } else {
+//     console.log("temp is moderate");
+    
+//}
+
+// const power=20;
+
+// if (power>=100) console.log(`It has high voltage `)
+// else  console.log("It ha low voltage");
+
+// //console.log(`It has high voltage ${V}`);
+
+// const balance=100;
+
+// if (balance<500) {
+//     console.log("it is less than 500");
+// } else if (balance<750) {
+//     console.log("it is less than 750");
+// } else if (balance<1200){
+//     console.log('it is less than 1200');
+    
+// }else{
+//     console.log("it is very high");
+    
+// }
+
+const userid=false;
+const loggedin=true;
+const havePassword=false;
+
+if (userid || loggedin || havePassword) {
+    console.log("Ready to use our app");
+}else{
+    console.log("cant use our app now ");
+    
+}
