4893a2201aa06d999fffefd10d1c28a45f33f886
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-17 06:28:54 +0000

    switch statement

---
+++ 03_basics/04_switchcase.js
@@ -0,0 +1,38 @@
+// const month="Jan";
+
+// switch (month) {
+//     case "Jan":
+//         console.log("Its January");
+//         break;
+//     case "feb":
+//         console.log("Its Feb");
+//         break;
+//     case "March":
+//         console.log("Its March");
+//         break;
+
+//     default:
+//         console.log("Not matched");
+        
+//         break;
+// }
+// const userEmail={Name:"Sahil"}
+
+// if (Object.keys(userEmail).length===0) {
+//     console.log("It is empty");
+    
+// } else {
+//     console.log("u r logged in");
+    
+// }
+
+// let val1;
+// val1=undefined??10;
+
+// console.log(val1);
+
+const ChickenPiece=10;
+
+ChickenPiece>=10 ? console.log("I enjoyed"):console.log("I am hungry");
+
+
