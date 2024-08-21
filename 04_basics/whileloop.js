dfbb325ebd980908a94de1aebca677e3f081a997
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-21 12:44:57 +0000

    do while loop added

---
+++ 04_basics/whileloop.js
@@ -0,0 +1,18 @@
+let i=0;
+while (i<=20) {
+  //  console.log(`value of i is ${i}`);
+    i=i+2;
+    
+}
+let myarr=['batman','superman','flash']
+let arr=0;
+while (arr<myarr.length) {
+   // console.log(` ${myarr[arr]}`);
+    arr=arr+1
+}
+let score=1;
+do {
+    console.log(`value of score is ${score}`);
+    score=score+1
+    
+} while (score<=20);
