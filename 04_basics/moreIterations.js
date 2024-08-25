181f0abac51326c93d89e8920e1baba490ad1eec
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-25 12:23:15 +0000

    moreIterationsAdded

--- 04_basics/moreIterations.js
+++ 04_basics/moreIterations.js
@@ -19,7 +19,9 @@ map.set('USA',"United States of America");
 
 //console.log(map);
 
-for (const [key,value] of map) {
+for (const key in map) {
+// console.log(`${map[key]}`); it is wrong method
+    
    // console.log(key,':-',value);
     
 }
@@ -30,6 +32,51 @@ const myObj={
 }
 
 for (const key in myObj) {
-    console.log(`${key} is the shortcut for ${myObj[key]}`);
+  //  console.log(`${key} is the shortcut for ${myObj[key]}`);
     
 }
+
+const programming=['java','ruby','c++','javascript','python']
+
+for (const key in programming) {
+//console.log(`the value of keys are ${programming[key]} `);
+    
+}
+
+const coding=['java','ruby','c++','javascript','python'];
+
+//coding.forEach(function (val){
+//console.log(`the value of coding is ${val}`);
+
+//});
+
+//coding.forEach((items)=>{
+  //  console.log(items);
+    
+//} )
+
+/**function printMe(items,index,arr) {
+    console.log(items,index,arr);
+}
+
+coding.forEach(printMe)**/
+
+const myCoding=[
+    {
+        languageName:"Java",
+        languageFile:".java"
+    },
+    {
+        languageName:"JavaScript",
+        languageFile:"js"
+    },
+    {
+        languageName:"cpp",
+        languageFile:".cpp"
+    }
+]
+
+myCoding.forEach((items) => {
+    console.log(items.languageFile);
+    
+});
