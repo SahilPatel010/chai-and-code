457cc5f5e0c8788e2e3a5046222006981eda6e62
SahilPatel010 <sahil9223538388@gmail.com>
2024-09-24 11:55:16 +0000

    promisses completed

--- 06_advance_one/promises.js
+++ 06_advance_one/promises.js
@@ -19,47 +19,92 @@
 // })
 
 
-// const promiseThree=new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve({
-//          userName:"Bruce" ,age:31,
+ const promiseThree=new Promise(function(resolve,reject){
+     setTimeout(function(){
+         resolve({
+          userName:"Bruce" ,age:31,
+          Profession:"Batman" 
+         })
+     },500)
+ })
+
+ promiseThree.then(function(statement){
+    console.log(statement);
+  })
+// const promiseFour=new Promise(function(resolve,reject){
+//  setTimeout(function(){
+//   let error=false;
+//   if (!error) {
+//     resolve({
+//         userName:"Bruce" ,age:31,
 //          Profession:"Batman" 
-//         })
-//     },2000)
+//     })
+//   } else {
+//     reject('Error:Something went wrong')
+//   }
+//  },2000)
+
 // })
 
-// promiseThree.then(function(statement){
-//     console.log(statement);
-    
+// promiseFour
+// .then((user)=>{
+// console.log(user);
+// return user.userName
 // })
-const promiseFour=new Promise(function(resolve,reject){
- setTimeout(function(){
-  let error=false;
-  if (!error) {
-    resolve({
-        userName:"Bruce" ,age:31,
-         Profession:"Batman" 
-    })
-  } else {
-    reject('Error:Something went wrong')
-  }
- },2000)
-
-})
-
-promiseFour
-.then((user)=>{
-console.log(user);
-return user.userName
-})
-.then((userName)=>{
-    console.log(userName);
+// .then((userName)=>{
+//     console.log(userName);
    
-})
-.catch((error)=>{
-    console.log(error);
+// })
+// .catch((error)=>{
+//     console.log(error);
     
-}).finally(()=>{console.log("The promise is either resolved or rejected");
+// }).finally(()=>{console.log("The promise is either resolved or rejected");
+// })
+
+// const promiseFive=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+//     let error=true;
+//       if (!error) {
+//          resolve({
+//              userName:"JavaScript",
+//               password:123
+//          })
+//        } else {
+//          reject('Error:Something went wrong')
+//        }
+//       },2000)
+    
+//      })
+
+
+// async function consumePromiseFive(){
+//    try {
+//     const response=await promiseFive
+//     console.log(response); 
+//    } catch (error) {
+//     console.log(error); 
+//    }
+// }
+
+// consumePromiseFive()
+/**async function getAllUser(){
+ try {
+  const response= await fetch("https://api.github.com/users/SahilPatel010") 
+const Data=response.json()
+  console.log(Data);
+ 
+ } catch (error) {
+console.log("E:",error);
+ }
+getAllUser()**/
+
+
+fetch("https://api.github.com/users/SahilPatel010")
+.then(function(response){
+  return response.json()
 })
-
-
+.then(function(data){
+console.log(data);
+})
+.catch((error)=>{console.log(error);
+})
