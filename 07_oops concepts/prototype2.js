fe9cc2dfa3a0b0e1fe8181c794eb118f3f7b2af8
SahilPatel010 <sahil9223538388@gmail.com>
2024-10-02 13:04:14 +0000

    new prototype added

---
+++ 07_oops concepts/prototype2.js
@@ -0,0 +1,63 @@
+// let myHeroes=["Batman","Ironman"]
+
+// let HePower={
+//     Batman:"rich",
+//     Ironman:"genius",
+
+//     getBatPower:function(){
+//         console.log(`the power is ${this.Batman}`);
+//     }
+// }
+
+// Object.prototype.Ayyanji=function(params) {
+//     console.log("Ayyanji is present in all object");
+    
+// }
+ 
+// indianHeroes=["Krish","G-One"]
+
+// Array.prototype.codePath=function () {
+//     console.log("code is running blank");
+// }
+
+// //HePower.codePath()
+
+// myHeroes.Ayyanji()
+// myHeroes.codePath()
+
+// indianHeroes.codePath()
+
+// const Teacher={
+//     makeVideo:true
+// }
+
+// const TeacherSupport={
+//     isAvailable:true
+// }
+
+// const TASupport={
+//     makeAssignment:'JS Assignment',
+//     fullTime:true
+// }
+
+// const user={
+//     userName:"Chai",
+//     email:"chai@google.com"
+// }
+
+// //Teacher.__proto__=user
+
+// Object.setPrototypeOf(Teacher,TASupport)
+
+// console.log(user.makeVideo);
+
+let anotherUserName="ChaiAurCode      "
+
+String.prototype.trueLength=function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`);
+    console.log(`true length:${this.trim().length}`);
+    
+}
+anotherUserName.trueLength()
+"bruce".trueLength()
